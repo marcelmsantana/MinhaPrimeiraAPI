3d7e787f379c600dc0cdeaa48bc958bddeabc7a3
Marcel Santana <marcel.santana@tecfy.com.br>
2022-11-21 11:12:00 -0300

    MyFirstCommit

---
+++ Camada_Negocio/Configuration/AutoMapperConfig.cs
@@ -0,0 +1,16 @@
+﻿using Camada_Negocio.Model;
+using AutoMapper;
+using Camada_Dados.Repository.Entity;
+
+namespace Web_Api.Configuration
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<Aluno, AlunoModel>().ReverseMap();
+            CreateMap<Escola, EscolaModel>().ReverseMap();
+            CreateMap<Professor, ProfessorModel>().ReverseMap();
+        }
+    }
+}
---
+++ TesteAPI_Completa/Startup.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TesteAPI_Completa.Data;
+using Microsoft.EntityFrameworkCore.Design;
+using AutoMapper;
+
+namespace Web_Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<MeuDbContext>(optionsAction: options =>
+               options.UseSqlServer(Configuration.GetConnectionString(name: "MeuDbContext")));
+
+            services.AddAutoMapper(typeof(Startup));
+
+            services.AddControllers();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
